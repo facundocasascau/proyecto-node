bca3ce01ea5e5615fca4457a55e351b557498515
Facundo Casas Cau <facundocasascau@gmail.com>
2021-08-03 22:22:49 -0300

    desafio clase  8

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { brotliDecompress } = require('zlib');
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Estamos ATR')
 })
 
 const server = app.listen(port, () => {
@@ -14,40 +15,79 @@ const server = app.listen(port, () => {
 server.on("error", error => console.log(`Ha ocurrido un error: ${error}`))
 const ubicacion = path.resolve(__dirname, './productos.json');
 
-let visitasItems = 0
-app.get('/items', (request, response) => {
-    visitasItems++
+
+//Listar en forma total (get) 
+app.get('/productos', (req, response) => {
     fs.readFile(ubicacion, 'utf-8', (error, data) => {
-        if (error) {
+        if (error || data.length == 0) {
+            response.json({
+                error: 'no hay productos cargados',
+            });
             return console.log('Ocurió un error', error);
+        } else {
+            let productos = JSON.parse(data);
+            response.json(productos);
         }
-        let productos = JSON.parse(data);
-        response.json(productos);
 
     });
- 
+
 });
 
-let visitasRandom = 0;
-app.get('/item-ramdon', (request, response) => {
-    visitasRandom++
+//Listar en forma individual (get)
+app.get('/productos/:id', (req, response) => {
+    const posicion = parseInt(req.params.id)
     fs.readFile(ubicacion, 'utf-8', (error, data) => {
-        if (error) {
-            return console.log('Ocurió un error', error);
-        }
         let productos = JSON.parse(data);
-        let aleatorio = Math.floor((Math.random() * productos.length))
-        
-        response.json({
-           item:productos[aleatorio].title,
+        if (posicion < 1 || posicion > productos.length) {
+            response.json({
+                error: 'producto no encontrado',
+            });
+        } else {
+            response.json({
+                item: productos[posicion - 1],
+            });
+        }
+    });
+});
+
+
+//Almacenar un producto (post)
+app.use(express.json());
+app.post('/guardar', (req, response) => {
+
+    const body = req.body;
+    if (
+        !body.title ||
+        !body.price ||
+        !body.thumbnail ||
+        typeof body.title != 'string' ||
+        typeof body.price != 'number' ||
+        typeof body.thumbnail != 'string'
+    ) {
+        return res.status(400).json({
+            msg: 'Ingrese title, price y url de foto por favor',
+        });
+    }
+    fs.readFile(ubicacion, 'utf-8', (error, data) => {
+        let productos = JSON.parse(data);
+        let obj = {
+            title: body.title,
+            price: body.price,
+            thumbnail: body.thumbnail,
+            id: productos.length + 1
+        };
+        productos.push(obj);
+        let objeto = JSON.stringify(productos);
+        fs.writeFile(ubicacion, objeto, (err, e) => {
+            if (err) {
+                console.log('Error al guardar', err);
+            } else {
+                console.log('Se guardó correctamente');
+                response.status(201).json({
+                    data: obj,
+                });
+            }
+        });
     });
 
 });
-});
-
-app.get('/visitas', (request, response) => {
-    response.json({
-        Items: `${visitasItems}`,
-        ItemsRandom: `${visitasRandom}`
-    });
-})
