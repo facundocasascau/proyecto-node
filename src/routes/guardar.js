d7196906ffbafaafe6e8e881fd68a027e088bc4c
Facundo Casas Cau <facundocasascau@gmail.com>
2021-08-14 22:03:39 -0300

    Continuando con guardar

--- src/routes/guardar.js
+++ src/routes/guardar.js
@@ -4,51 +4,61 @@ const router = express.Router();
 
 const path = require('path');
 const ubicacion = path.resolve(__dirname, '../productos.json');
+// const app = express();
+// router.use(express.urlencoded({ extended: true }));
 
-
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 //Almacenar un producto (post)
+router.get('/', (req, response) => {
+  response.render('guardar.pug')
+  const body = req.body;
+  console.log(body)
+  console.log(response.originalUrl)
+})
 
 router.post('/', (req, response) => {
-
   const body = req.body;
-  if (
-    !body.title ||
-    !body.price ||
-    !body.thumbnail ||
-    typeof body.title != 'string' ||
-    //typeof body.price != 'number' ||
-    typeof body.thumbnail != 'string'
-  ) {
-    return response.status(400).json({
-      msg: 'Ingrese title, price y url de foto por favor',
-    });
-  }
-
-  fs.readFile(ubicacion, 'utf-8', (error, data) => {
-    let productos = JSON.parse(data);
-    let idsProductos = [];
-    productos.forEach(e => idsProductos.push(e.id))
-    console.log(productos)
-    let max = Math.max(...idsProductos);
-    let obj = {
-      title: body.title,
-      price: body.price,
-      thumbnail: body.thumbnail,
-      id: max + 1
-    };
-    productos.push(obj);
-    let objeto = JSON.stringify(productos);
-    fs.writeFile(ubicacion, objeto, (err, e) => {
-      if (err) {
-        console.log('Error al guardar', err);
-      } else {
-        console.log('Se guardó correctamente');
-        response.render('guardar.pug',{products: productos})
-      }
-    });
-  });
-
+  console.log(body)
+  console.log(response)
+  // if (
+  //   !body.title ||
+  //   !body.price ||
+  //   !body.thumbnail ||
+  //   typeof body.title != 'string' ||
+  //   //typeof body.price != 'number' ||
+  //   typeof body.thumbnail != 'string'
+  // ) {
+  //   console.log(body)
+  //   response.render('guardar.pug')
+  // } else {
+  //
+  //   fs.readFile(ubicacion, 'utf-8', (error, data) => {
+  //     let productos = JSON.parse(data);
+  //     let idsProductos = [];
+  //     productos.forEach(e => idsProductos.push(e.id))
+  //     console.log(productos)
+  //     let max = Math.max(...idsProductos);
+  //     let obj = {
+  //       title: body.title,
+  //       price: body.price,
+  //       thumbnail: body.thumbnail,
+  //       id: max + 1
+  //     };
+  //     productos.push(obj);
+  //     let objeto = JSON.stringify(productos);
+  //     fs.writeFile(ubicacion, objeto, (err, e) => {
+  //       if (err) {
+  //         console.log('Error al guardar', err);
+  //       } else {
+  //         console.log('Se guardó correctamente');
+  //         response.render('guardar.pug',{products: productos})
+  //       }
+  //     });
+  //   });
+  // }
 });
 
 export default router;
